029f9c1f74d483016d0a34b4061e8da4f8372693
Brent Cameron <bjcameron@gmail.com>
2020-10-27 16:22:38 -0600

    fixed sign in by routing through edit instead of new so that params contains id; sign out still not working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,6 @@
 class SessionsController < ApplicationController
 
     def new
-        reset_session
         @user = User.find(params[:id])
         
         unless (user.nil?)
@@ -14,12 +13,26 @@ class SessionsController < ApplicationController
         redirect_to '/events/new'
     end
 
+    def edit
+        @user = User.find(params[:id])
+        
+        unless (@user.nil?)
+            session[:current_user_id] = @user.id
+            session[:current_user_name] = @user.name
+            session[:current_user] = @user
+        else
+
+        end
+        redirect_to '/events/new'
+    end
+
     def create 
         # session[:current_user_id] = @user.id
         # session[:current_user_name] = @user.name
     end
 
     def destroy
+        id = params[:id]
         reset_session
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :events
   resources :users
-  resources :sessions, only: [:new, :create, :destroy] 
+  resources :sessions, only: [:new, :create, :edit, :destroy] 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root 'users#sign_in'
