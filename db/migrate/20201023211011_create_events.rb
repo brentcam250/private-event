6c9438f14e3a7902ebeeb11d47378066e0ecef4b
Brent Cameron <bjcameron@gmail.com>
2020-10-28 16:25:57 -0600

    looks like the models/migrations are working this time

--- db/migrate/20201023211011_create_events.rb
+++ db/migrate/20201023211011_create_events.rb
@@ -2,7 +2,7 @@ class CreateEvents < ActiveRecord::Migration[5.2]
   def change
     create_table :events do |t|
       t.string :name
-      t.belongs_to :user
+      t.belongs_to :creator, foreign_key: { to_table: :users }, index: true, null: false
       t.datetime :event_time
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_23_211011) do
+ActiveRecord::Schema.define(version: 2020_10_28_221142) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
-    t.integer "user_id"
+    t.integer "creator_id", null: false
     t.datetime "event_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_events_on_user_id"
+    t.index ["creator_id"], name: "index_events_on_creator_id"
+  end
+
+  create_table "invitations", force: :cascade do |t|
+    t.integer "attendee_id", null: false
+    t.integer "attended_event_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["attended_event_id"], name: "index_invitations_on_attended_event_id"
+    t.index ["attendee_id"], name: "index_invitations_on_attendee_id"
   end
 
   create_table "users", force: :cascade do |t|
